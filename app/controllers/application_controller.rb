e09216cf3736804f0b94340cbd4d339d53baae88
lokeshsriramaneni <slokeshnaidu28@gmail.com>
2017-05-08 08:26:14 +0530

    added puts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user
-
+  puts "Hello"
   def authenticate_user
      if session[:user_id].blank?
          redirect_to sign_in_users_path
